3492caaa32e60213342d49c8fa9bd846d194fe90
elf <elff1@21cn.com>
2012-03-01 19:26:57 +0800

    initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstMine::Application.config.secret_token = '37c0288ccf405464f22e22fd4783fafb0d9cc5d4779d65afc214cc157fbed9aa5aaec7fe87b1d618a3217394233b70e190158aee2cf8fc683f029be91d4675e3'
